b53378355e03791857397e2885730ba43c17851c
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-17 15:57:14 +0300

    add routes and create home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,66 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import logo from './image/icons/logo.svg';
+import vote from './image/vote-table.png';
+import breed from './image/pet-breeds.png';
+import search from './image/images-search.png';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
 
 export default function App() {
-   return <div></div>;
+   const addActivePage = e => {
+      removeActivePage();
+      e.currentTarget.classList.add('link--active');
+   };
+   const removeActivePage = () => {
+      const links = document.querySelectorAll('.link');
+      links.forEach(link => link.classList.remove('link--active'));
+   };
+   return (
+      <div className="App">
+         <div className="Side-bar">
+            <Link to="/" className="logo-link" onClick={removeActivePage}>
+               <img className="logo-icon" src={logo} alt="logo" />
+               PetsPaw
+            </Link>
+            <h1 className="Title">Hi intern!</h1>
+            <p className="Text">Welcome to MI 2022 Front-end test</p>
+            <h2 className="titel-h2">Lets start using The Cat API</h2>
+            <nav className="nav">
+               <Link to="voting" className="link" onClick={addActivePage}>
+                  <div className="link-img link-img--vote">
+                     <img src={vote} alt="vote-table" />
+                  </div>
+
+                  <button className="link-btn" type="button">
+                     voting
+                  </button>
+               </Link>
+               <Link to="breed" className="link" onClick={addActivePage}>
+                  <div className="link-img link-img--breed">
+                     <img src={breed} alt="pet-breeds" />
+                  </div>
+
+                  <button className="link-btn" type="button">
+                     breheds
+                  </button>
+               </Link>
+               <Link to="gallery" className="link" onClick={addActivePage}>
+                  <div className="link-img link-img--gallery">
+                     <img src={search} alt="images-search" />
+                  </div>
+
+                  <button className="link-btn" type="button">
+                     gallery
+                  </button>
+               </Link>
+            </nav>
+         </div>
+         <Routes>
+            <Route path="/" element={<HomePage>home</HomePage>} />
+            <Route path="voting" element={<div>voting</div>} />
+            <Route path="breed" element={<div>breed</div>} />
+            <Route path="gallery" element={<div>gallery</div>} />
+         </Routes>
+      </div>
+   );
 }
