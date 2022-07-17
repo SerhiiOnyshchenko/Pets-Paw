830378c79b5d87007d6143bd895704f1293b32b2
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-17 15:55:02 +0300

    add images and create pages

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.css';
+
+export default function HomePage() {
+   return (
+      <div className="home-page">
+         <div className="Rectangle"></div>
+         <div className="img-bg"></div>
+      </div>
+   );
+}
