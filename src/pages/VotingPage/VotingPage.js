8d99a3aafe221d14ea4432785782f7de9afe8d43
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-17 18:33:51 +0300

    feat: voting page and add styles

--- src/pages/VotingPage/VotingPage.js
+++ src/pages/VotingPage/VotingPage.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import './VotingPage.css';
+import Container from './../../components/Container/Container';
+import SearchBar from './../../components/SearchBar/SearchBar';
+import BackButton from './../../components/BackButton/BackButton';
+import ButtonInfo from './../../components/ButtonInfo/ButtonInfo';
 
 export default function VotingPage() {
-   return <div>VotingPage</div>;
+   return (
+      <Container>
+         <SearchBar />
+         <div className="vote-box">
+            <div className="vote-top">
+               <BackButton />
+               <ButtonInfo>voting</ButtonInfo>
+            </div>
+         </div>
+      </Container>
+   );
 }
