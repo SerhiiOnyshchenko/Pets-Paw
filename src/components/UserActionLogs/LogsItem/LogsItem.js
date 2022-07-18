fecf3c370a2ea1f840201f84735ef493bbb8433a
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-18 11:02:36 +0300

    fix: user action log, add icon

--- src/components/UserActionLogs/LogsItem/LogsItem.js
+++ src/components/UserActionLogs/LogsItem/LogsItem.js
@@ -4,8 +4,13 @@ function LogsItem({ id, time, value }) {
    return (
       <li className="log-item">
          <span className="log-time">{time}</span>Image ID:{' '}
-         <span className="log-id"> {id} </span> was added to
-         {value ? 'Likes' : 'Dislikes'}{' '}
+         <span className="log-id">{id}</span> was added to{' '}
+         {value ? 'Likes' : 'Dislikes'}
+         {value ? (
+            <span className="log-icon log-icon--like"></span>
+         ) : (
+            <span className="log-icon log-icon--dislike"></span>
+         )}
       </li>
    );
 }
