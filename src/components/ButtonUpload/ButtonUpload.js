f76dc874574cf1c2c2aa52f90e5198eb31d85fa5
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-19 10:03:08 +0300

    feat: button upload component

---
+++ src/components/ButtonUpload/ButtonUpload.js
@@ -0,0 +1,10 @@
+import './ButtonUpload.css';
+
+export default function ButtonUpload() {
+   return (
+      <button className="btn-upload" type="button">
+         <span className="btn-upload-icon"></span>
+         upload
+      </button>
+   );
+}
