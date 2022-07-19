5b1368b4f43c37125d9ec4f36c29438b156ff8d5
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-19 20:38:46 +0300

    fix: gallery page. add functions on buttons

--- src/pages/GalleryPage/GalleryPage.js
+++ src/pages/GalleryPage/GalleryPage.js
@@ -6,32 +6,62 @@ import ButtonInfo from './../../components/ButtonInfo/ButtonInfo';
 import ButtonSelect from './../../components/ButtonSelect/ButtonSelect';
 import GalleryGrid from './../../components/GalleryGrid/GalleryGrid';
 import ButtonUpload from '../../components/ButtonUpload/ButtonUpload';
-import { getBreedImages } from '../../services/api';
+import { getCategories, votingRandomImage } from '../../services/api';
 import './GalleryPage.css';
 
 export default function GalleryPage() {
    const [breedImages, setBreedImages] = useState([]);
    const [limit, setLimit] = useState('5 items per page');
+   const [order, setOrder] = useState('Random');
+   const [type, setType] = useState('All');
+   const [breeds, setBreeds] = useState('All breeds');
+   const [categories, setCategories] = useState([
+      { id: '', name: 'All breeds' },
+   ]);
 
    useEffect(() => {
       fetchBreeds();
+      fetchCategories();
    }, []);
 
-   useEffect(() => {
-      const numLimit = limit.split(' ')[0];
-      fetchBreeds(numLimit);
-   }, [limit]);
-
-   const fetchBreeds = async limit => {
+   const fetchBreeds = async (limit, order, type, breedId) => {
       try {
-         const data = await getBreedImages(limit);
+         const data = await votingRandomImage(limit, order, type, breedId);
          setBreedImages(data);
       } catch (error) {
          console.log(error);
       }
    };
-   let arrSelect = ['All breeds'];
-   breedImages.forEach(el => arrSelect.push(el.name));
+
+   const fetchCategories = async () => {
+      try {
+         const data = await getCategories();
+         const datArr = [...data].map(el => {
+            return { id: el.id, name: el.name };
+         });
+         datArr.unshift({ id: '', name: 'All breeds' });
+         setCategories(datArr);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   const updateFilter = () => {
+      const numLimit = limit.split(' ')[0];
+      const typeArr = [];
+      if (type === 'Static') {
+         typeArr.push('jpg');
+         typeArr.push('png');
+      } else if (type === 'Animated') {
+         typeArr.push('gif');
+      } else {
+         typeArr.push('');
+      }
+      const [breedID] = categories.filter(el => el.name === breeds);
+      fetchBreeds(numLimit, order, typeArr, breedID?.id);
+   };
+
+   const arrSelect = [...categories].map(el => el.name);
    const limites = [
       '5 items per page',
       '10 items per page',
@@ -53,44 +83,52 @@ export default function GalleryPage() {
                <div className="filter-item">
                   <p className="filter-item-title">order</p>
                   <ButtonSelect
+                     main={order}
                      options={orderArr}
                      width={290}
                      bgcolor={'#ffffff'}
                      id={'order'}
-                     setSort={() => {}}
+                     setSort={setOrder}
                   />
                </div>
                <div className="filter-item">
                   <p className="filter-item-title">type</p>
                   <ButtonSelect
+                     main={type}
                      options={typeArr}
                      id={'type'}
                      width={290}
                      bgcolor={'#ffffff'}
-                     setSort={() => {}}
+                     setSort={setType}
                   />
                </div>
                <div className="filter-item">
                   <p className="filter-item-title">breed</p>
                   <ButtonSelect
+                     main={breeds}
                      options={arrSelect}
                      width={290}
                      id={'breed'}
                      bgcolor={'#ffffff'}
-                     setSort={() => {}}
+                     setSort={setBreeds}
                   />
                </div>
                <div className="filter-item">
                   <p className="filter-item-title">limit</p>
                   <div style={{ display: 'flex' }}>
                      <ButtonSelect
+                        main={limit}
                         options={limites}
                         width={240}
                         bgcolor={'#ffffff'}
                         id={'limit'}
                         setSort={setLimit}
                      />
-                     <button className="btn-update" type="button"></button>
+                     <button
+                        className="btn-update"
+                        type="button"
+                        onClick={updateFilter}
+                     ></button>
                   </div>
                </div>
             </div>
