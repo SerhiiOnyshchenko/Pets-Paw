f33c63d9e53686b98deff1740fb87c989701686c
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-20 23:27:07 +0300

    fix: Dislikes Page, add loader, get and delete images

--- src/pages/DislikesPage/DislikesPage.js
+++ src/pages/DislikesPage/DislikesPage.js
@@ -1,14 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BackButton from '../../components/BackButton/BackButton';
 import ButtonInfo from '../../components/ButtonInfo/ButtonInfo';
 import Container from '../../components/Container/Container';
+import DefaultState from '../../components/DefaultState/DefaultState';
 import GalleryGrid from '../../components/GalleryGrid/GalleryGrid';
+import Loader from '../../components/Loader/Loader';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import {
+   deleteVoteImage,
+   getImagesById,
+   getVoteHistory,
+} from '../../services/api';
 import './DislikesPage.css';
 
 export default function DislikesPage({ search, setSearch }) {
    const [breedImages, setBreedImages] = useState([]);
+   const [loader, setLoader] = useState(false);
 
+   useEffect(() => {
+      histList();
+   }, []);
+
+   const histList = async () => {
+      setLoader(true);
+      try {
+         const data = await getVoteHistory();
+         const likeArr = [];
+         for (const el of data) {
+            if (el.value === 0) {
+               const data = await getImagesById(el.image_id);
+               likeArr.push({ ...el, url: data.url });
+            }
+         }
+         setBreedImages(likeArr);
+      } catch (err) {
+         console.log(err);
+      }
+      setLoader(false);
+   };
+
+   const removeLikes = async id => {
+      try {
+         await deleteVoteImage(id);
+         await histList();
+      } catch (err) {
+         console.log(err);
+      }
+   };
    return (
       <Container>
          <SearchBar search={search} setSearch={setSearch} active={'dislikes'} />
@@ -17,7 +55,13 @@ export default function DislikesPage({ search, setSearch }) {
                <BackButton />
                <ButtonInfo>dislikes</ButtonInfo>
             </div>
-            <GalleryGrid images={breedImages} click={() => {}} />
+            {loader ? (
+               <Loader />
+            ) : breedImages.length === 0 ? (
+               <DefaultState />
+            ) : (
+               <GalleryGrid images={breedImages} click={removeLikes} />
+            )}
          </div>
       </Container>
    );
