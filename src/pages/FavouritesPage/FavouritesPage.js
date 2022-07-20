ec793aa100c6e83e5097128ea26b4d1d645bf389
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-20 23:27:45 +0300

    fix: Favourites Page, add loader, get and delete images

--- src/pages/FavouritesPage/FavouritesPage.js
+++ src/pages/FavouritesPage/FavouritesPage.js
@@ -1,13 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BackButton from '../../components/BackButton/BackButton';
 import ButtonInfo from '../../components/ButtonInfo/ButtonInfo';
 import Container from '../../components/Container/Container';
+import DefaultState from '../../components/DefaultState/DefaultState';
 import GalleryGrid from '../../components/GalleryGrid/GalleryGrid';
+import Loader from '../../components/Loader/Loader';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import { deleteFavouritesImage, getFavouritesImage } from '../../services/api';
 import './FavouritesPage.css';
 
 export default function FavouritesPage({ search, setSearch }) {
    const [breedImages, setBreedImages] = useState([]);
+   const [loader, setLoader] = useState(false);
+
+   useEffect(() => {
+      fetchFavouritesImage();
+   }, []);
+
+   const fetchFavouritesImage = async () => {
+      setLoader(true);
+      try {
+         const data = await getFavouritesImage();
+         setBreedImages(data);
+      } catch (error) {
+         console.log(error);
+      }
+      setLoader(false);
+   };
+
+   const removeFavouritesImage = async id => {
+      try {
+         await deleteFavouritesImage(id);
+         await fetchFavouritesImage();
+      } catch (error) {
+         console.log(error);
+      }
+   };
 
    return (
       <Container>
@@ -17,7 +45,16 @@ export default function FavouritesPage({ search, setSearch }) {
                <BackButton />
                <ButtonInfo>Favourites</ButtonInfo>
             </div>
-            <GalleryGrid images={breedImages} click={() => {}} />
+            {loader ? (
+               <Loader />
+            ) : breedImages.length === 0 ? (
+               <DefaultState />
+            ) : (
+               <GalleryGrid
+                  images={breedImages}
+                  click={removeFavouritesImage}
+               />
+            )}
          </div>
       </Container>
    );
