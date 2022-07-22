c072b4e42f590ef6513ff1c186e9f51fbe9d0220
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-07-22 22:52:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-      <BrowserRouter basename="pets-paw-intern.netlify.app/">
+      <BrowserRouter>
          <App />
       </BrowserRouter>
    </React.StrictMode>
